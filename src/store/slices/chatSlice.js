827b5897268cf945dba19079271e1db58e7436a2
Alexander Bulgakov <anbulgakov@gmail.com>
2023-05-27 16:11:47 +0300

    logs deleted

--- src/store/slices/chatSlice.js
+++ src/store/slices/chatSlice.js
@@ -3,25 +3,17 @@ import axios from 'axios';
 
 export const sendMessage = createAsyncThunk('chat/sendMessage', async (text, { getState }) => {
   const { chat } = getState();
-  // console.log('text from send', text);
-  // console.log('chatState fom send', chat);
-
   const contact = chat.contacts.find((contact) => contact.id === chat.activeContactId);
-  // console.log('contact form send', contact);
-
   const body = {
     chatId: `${contact.phoneNumber}@c.us`,
     message: text,
   };
 
   try {
-    // console.log('thunk', chat);
-
     const { data } = await axios.post(
       `https://api.green-api.com/waInstance${chat.idInstance}/SendMessage/${chat.apiTokenInstance}`,
       body,
     );
-
     return { data, text };
   } catch (e) {
     console.log(e);
@@ -31,11 +23,9 @@ export const sendMessage = createAsyncThunk('chat/sendMessage', async (text, { g
 export const receiveNotification = createAsyncThunk('chat/receiveNotification', async (arg, { getState }) => {
   try {
     const { chat } = getState();
-
     const data = await axios.get(
       `https://api.green-api.com/waInstance${chat.idInstance}/receiveNotification/${chat.apiTokenInstance}`,
     );
-
     return data;
   } catch (e) {
     console.log(e);
@@ -48,8 +38,6 @@ const deleteNotification = async (id, idInstance, apiTokenInstance) => {
       `https://api.green-api.com/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${id}
       `,
     );
-    // console.log('deleteNotification >>> ', data);
-
     return data;
   } catch (e) {
     console.log(e);
@@ -68,45 +56,33 @@ const chatSlice = createSlice({
   initialState,
   reducers: {
     setReqParameters: (state, action) => {
-      // console.log('payload >>> ', action.payload);
       state.idInstance = action.payload.idInstance;
       state.apiTokenInstance = action.payload.apiTokenInstance;
     },
     logout: (state, action) => {
-      // console.log(action);
       state = initialState;
     },
     addContact: (state, action) => {
       state.contacts.push(action.payload);
-      // console.log(state.contacts);
     },
     setActiveContact: (state, action) => {
-      // console.log('contacts from set', state.contacts);
       state.activeContact = state.contacts.find((contact) => contact.id === action.payload);
       state.activeContactId = action.payload;
-      // console.log('state.activeContactId ', state.activeContactId);
-      // console.log('store >> ', state.activeContact.phoneNumber);
     },
   },
   extraReducers: {
     [sendMessage.fulfilled]: (state, action) => {
-      // console.log('text', action.payload.text);
-      // console.log('data', action.payload.data);
       const contact = state.contacts.find((contact) => contact.id === state.activeContactId);
       contact.messages.push({ side: 'send', text: action.payload.text });
     },
     [receiveNotification.fulfilled]: (state, action) => {
-      // console.log('receive >>> ', action.payload.data);
       if (!!action.payload.data) {
         const contact = state.contacts.find((contact) => contact.id === state.activeContactId);
-        // console.log('contact from receive full', contact);
         contact.messages.push({
           side: 'incoming',
           text: action.payload.data?.body?.messageData?.textMessageData?.textMessage,
         });
-        // console.log('id >>> ', action.payload.data?.receiptId);
         deleteNotification(action.payload.data?.receiptId, state.idInstance, state.apiTokenInstance);
-        // console.log('receiveNotification data', action.payload.data);
       }
     },
     [deleteNotification.fulfilled]: (state, action) => {
@@ -129,6 +105,5 @@ export const activeContact = (state) => state.chat.activeContact;
 
 export const getMessages = (state) => {
   const contact = state.chat.contacts.find((contact) => contact.id == state.chat.activeContactId);
-  // console.log('contact from messages', contact);
   return !!contact ? contact.messages : [];
 };
